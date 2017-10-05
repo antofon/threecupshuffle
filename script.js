489266206dff2a40e3496e457f05f0e0ff428f35
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-05 14:27:43 -0700

    Hide appropriate content on clicks
    Will show and hide content based on what buttons the user selects.

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ document.addEventListener("DOMContentLoaded", function(){
   console.log("DOMContent is properly loaded;");
 
   var hideRules = document.getElementById('rules');
+  var showRules = document.getElementById('fixed-rules');
+  var showCups = document.getElementById('begin');
   var play = document.getElementById('play');
 
   play.addEventListener('click', function() {
     hideRules.style.display = 'none';
     play.style.display = 'none';
+    showCups.style.display = 'block';
+    showRules.style.display = 'block';
+  });
+
+  showRules.addEventListener('click', function () {
+    hideRules.style.display = 'block';
+    play.style.display = 'block';
+    showCups.style.display = 'none';
+    showRules.style.display = 'none';
   });
 
 
