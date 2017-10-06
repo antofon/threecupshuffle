0ababc2d0a37128bbb161a9bb66d712a23f30e09
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-06 15:37:09 -0700

    Added certain interactions based upon user clicks
    Console.log message pops up as well as position location. For testing
    purposes. Functions added pertaining to the button of each cup.

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
   "use strict";
-  console.log("DOMContent is properly loaded;");
+  console.log("DOMContent is properly loaded");
 
+  var cup1Button = document.getElementById('cup1Button');
+  var cup2Button = document.getElementById('cup2Button');
+  var cup3Button = document.getElementById('cup3Button');
   var hideRules = document.getElementById('rules');
+  var play = document.getElementById('play');
   var showRules = document.getElementById('fixed-rules');
   var showCups = document.getElementById('begin');
-  var play = document.getElementById('play');
+  var positionChoice = 0;
+
+  cup1Button.addEventListener('click',function() {
+    positionChoice = (Math.floor(Math.random() * 3)) + 1;
+    console.log('cup1 here!');
+    console.log('This is position: ' + positionChoice);
+  });
+
+  cup2Button.addEventListener('click', function() {
+    positionChoice = (Math.floor(Math.random() * 3)) + 1;
+    console.log('cup2 here!');
+    console.log('This is position: ' + positionChoice);
+  });
+
+  cup3Button.addEventListener('click', function() {
+    positionChoice = (Math.floor(Math.random() * 3)) + 1;
+    console.log('cup3 here!');
+    console.log('This is position: ' + positionChoice);
+  });
 
   play.addEventListener('click', function() {
     hideRules.style.display = 'none';
@@ -20,8 +42,6 @@ document.addEventListener("DOMContentLoaded", function(){
     showCups.style.display = 'none';
     showRules.style.display = 'none';
   });
-
-
 
 
 });
