22e9e6aa89037d2f65edf5838a6ace2b0aad4958
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-07 16:50:18 -0700

    Correct ball appears which 'correct' message below
    Appears when user guesses the ball's location correctly.

--- script.js
+++ script.js
@@ -15,26 +15,37 @@ document.addEventListener("DOMContentLoaded", function(){
   var play = document.getElementById('play');
   var showRules = document.getElementById('fixed-rules');
   var showCups = document.getElementById('begin');
+  //will be used for random position of ball
   var positionChoice = 0;
 
   cup1Button.addEventListener('click',function() {
 
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
+    //if the user's guess equals the random position of the ball
     if(firstPos === positionChoice) {
-      // var buttonElt = document.getElementsByTagName('button')[0];
-      // var replaceButton = document.createElement('span');
-      // replaceButton.innerHTML = buttonElt.innerHTML;
-      // buttonElt.parentNode.replaceChild(replaceButton,buttonElt);
-      // console.log(buttonElt);
-      console.log('Correct! Your position is: ' + firstPos + ' and the ball is also at position: ' + positionChoice);
+      //accesses second element(note, position 0 is the first element)
+      var buttonElt = document.getElementsByTagName('button')[1];
+      //create new element (span)
+      var replaceButton = document.createElement('span');
 
+      //store the button's contents into replaceButton var (which will be used)
+      replaceButton.innerHTML = buttonElt.innerHTML;
+      //replace the child element of the parent node. Now we are replacing the old element's info (buttonElt), with the new element's info (replaceButton)
+      buttonElt.parentNode.replaceChild(replaceButton,buttonElt);
+      //modifying new element's text
+      replaceButton.innerHTML = 'Correct!';
+      //adding the 'correct' class
+      replaceButton.className += 'correct';
+      replaceButton.style.fontSize = '1.8em';
+      replaceButton.style.paddingTop = '40px';
+
+      //access 'src' attribute of cup1 ID (which is for the first red cup) and overwrite with ball SVG
       cup1.src = 'metallic_ball.svg';
       cup1.style.width = '100px';
       cup1.style.height = '100px';
       cup1.style.marginTop = '80px';
 
-      // cup1Button.innerHTML = 'Correct!';
-      // cup1Button.className += 'correct';
+      console.log('Correct! Your position is: ' + firstPos + ' and the ball is also at position: ' + positionChoice);
     }
 
     else {
@@ -72,6 +83,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // console.log('This is position: ' + positionChoice);
   });
 
+  //when the 'Let's Play' button is pressed, hide the rules and show the cups'
   play.addEventListener('click', function() {
     hideRules.style.display = 'none';
     play.style.display = 'none';
@@ -79,6 +91,7 @@ document.addEventListener("DOMContentLoaded", function(){
     showRules.style.display = 'block';
   });
 
+  //when the 'Show Rules' button is pressed, hide the cups and show the rules'
   showRules.addEventListener('click', function () {
     hideRules.style.display = 'block';
     play.style.display = 'block';
