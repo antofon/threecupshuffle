2bd6895c2341e1a823f09bda26b073d2ef5fa945
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-07 16:06:23 -0700

    Added conditions to compare choices
    Log message printed to console determining if the user's guess is the
    same as the random guess, which is the location of the ball (integer).

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded", function(){
   var cup1Button = document.getElementById('cup1Button');
   var cup2Button = document.getElementById('cup2Button');
   var cup3Button = document.getElementById('cup3Button');
+  var cup1 = document.getElementById('cup1');
+  var cup2 = document.getElementById('cup2');
+  var cup3 = document.getElementById('cup3');
+  var firstPos = 1;
+  var secondPos = 2;
+  var thirdPos = 3;
   var hideRules = document.getElementById('rules');
   var play = document.getElementById('play');
   var showRules = document.getElementById('fixed-rules');
@@ -13,20 +19,43 @@ document.addEventListener("DOMContentLoaded", function(){
 
   cup1Button.addEventListener('click',function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
-    console.log('cup1 here!');
-    console.log('This is position: ' + positionChoice);
+    if(firstPos === positionChoice) {
+      console.log('Correct! Your position is: ' + firstPos + ' and the ball is also at position: ' + positionChoice);
+    }
+
+    else {
+      console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
+    }
+    // console.log('cup1 here!');
+    // console.log('This is position: ' + positionChoice);
   });
 
   cup2Button.addEventListener('click', function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
-    console.log('cup2 here!');
-    console.log('This is position: ' + positionChoice);
+    if(secondPos === positionChoice) {
+      console.log('Correct! Your position is: ' + secondPos + ' and the ball is also at position: ' + positionChoice);
+    }
+
+    else {
+      console.log('Incorrect! Your position is: ' + secondPos + ' and the ball is at position: ' + positionChoice);
+    }
+    // console.log('cup2 here!');
+    // console.log('This is position: ' + positionChoice);
   });
 
   cup3Button.addEventListener('click', function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
-    console.log('cup3 here!');
-    console.log('This is position: ' + positionChoice);
+    if(thirdPos === positionChoice) {
+      console.log('Correct! Your position is: ' + thirdPos + ' and the ball is also at position: ' + positionChoice);
+    }
+
+    else {
+      console.log('Incorrect! Your position is: ' + thirdPos + ' and the ball is at position: ' + positionChoice);
+    }
+
+
+    // console.log('cup3 here!');
+    // console.log('This is position: ' + positionChoice);
   });
 
   play.addEventListener('click', function() {
