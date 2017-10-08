dcb10bee67617d5728615fd04bc2d7b493c1f039
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-07 17:34:29 -0700

    Added conditions for each of cups (need reset game)
    Conditions added, but they conflict between cup2 and cup3. Most likely
    due to continuous clicking of buttons going into the if-else conditions
    and altering the button data. Reset game button needed (after errors
    taken care of)

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function(){
   //will be used for random position of ball
   var positionChoice = 0;
 
-  cup1Button.addEventListener('click',function() {
 
+  cup1Button.addEventListener('click',function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
     //if the user's guess equals the random position of the ball
     if(firstPos === positionChoice) {
-      //accesses second element(note, position 0 is the first element)
+      //accesses second element(note, position 0 is the first element, which is the 'Show Rules' button)
       var buttonElt = document.getElementsByTagName('button')[1];
       //create new element (span)
       var replaceButton = document.createElement('span');
@@ -49,39 +49,107 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     else {
-      console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
-    }
-    // console.log('cup1 here!');
-    // console.log('This is position: ' + positionChoice);
+      //same result as 'if' statement, but do not display ball, only 'incorrect' message
+      var buttonElt = document.getElementsByTagName('button')[1];
+      var replaceButton = document.createElement('span');
+
+      replaceButton.innerHTML = buttonElt.innerHTML;
+      buttonElt.parentNode.replaceChild(replaceButton,buttonElt);
+      replaceButton.innerHTML = 'Incorrect!';
+      replaceButton.className += 'incorrect';
+      replaceButton.style.fontSize = '1.8em';
+      replaceButton.style.paddingTop = '40px';
+
+      //cup1.style.display = 'none';
+    //   var playAgain = prompt("Would you like to play again? (Yes or No)");
+    //   if(playAgain.toUpperCase() === 'YES') {
+    //     alert("Great! Please refresh the page to begin a new game.");
+    //   }
+    //
+    //   else {
+    //     alert("Okay, see you next time!");
+    //   }
+    //   console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
+     }
   });
+
 
   cup2Button.addEventListener('click', function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
     if(secondPos === positionChoice) {
+      var buttonElt2 = document.getElementsByTagName('button')[2];
+      var replaceButton2 = document.createElement('span');
+
+      replaceButton2.innerHTML = buttonElt2.innerHTML;
+      buttonElt2.parentNode.replaceChild(replaceButton2,buttonElt2);
+      replaceButton2.innerHTML = 'Correct!';
+      replaceButton2.className += 'correct';
+      replaceButton2.style.fontSize = '1.8em';
+      replaceButton2.style.paddingTop = '40px';
+
+      cup2.src = 'metallic_ball.svg';
+      cup2.style.width = '100px';
+      cup2.style.height = '100px';
+      cup2.style.marginTop = '80px';
+
       console.log('Correct! Your position is: ' + secondPos + ' and the ball is also at position: ' + positionChoice);
     }
 
     else {
+      //same result as 'if' statement, but do not display ball, only 'incorrect' message
+      var buttonElt2 = document.getElementsByTagName('button')[2];
+      var replaceButton2 = document.createElement('span');
+
+      replaceButton2.innerHTML = buttonElt2.innerHTML;
+      buttonElt2.parentNode.replaceChild(replaceButton2,buttonElt2);
+      replaceButton2.innerHTML = 'Incorrect!';
+      replaceButton2.className += 'incorrect';
+      replaceButton2.style.fontSize = '1.8em';
+      replaceButton2.style.paddingTop = '40px';
+
       console.log('Incorrect! Your position is: ' + secondPos + ' and the ball is at position: ' + positionChoice);
     }
-    // console.log('cup2 here!');
-    // console.log('This is position: ' + positionChoice);
   });
+
 
   cup3Button.addEventListener('click', function() {
     positionChoice = (Math.floor(Math.random() * 3)) + 1;
     if(thirdPos === positionChoice) {
+      var buttonElt3 = document.getElementsByTagName('button')[3];
+      var replaceButton3 = document.createElement('span');
+
+      replaceButton3.innerHTML = buttonElt3.innerHTML;
+      buttonElt3.parentNode.replaceChild(replaceButton3,buttonElt3);
+      replaceButton3.innerHTML = 'Correct!';
+      replaceButton3.className += 'correct';
+      replaceButton3.style.fontSize = '1.8em';
+      replaceButton3.style.paddingTop = '40px';
+
+      cup3.src = 'metallic_ball.svg';
+      cup3.style.width = '100px';
+      cup3.style.height = '100px';
+      cup3.style.marginTop = '80px';
+
       console.log('Correct! Your position is: ' + thirdPos + ' and the ball is also at position: ' + positionChoice);
     }
 
     else {
+      //same result as 'if' statement, but do not display ball, only 'incorrect' message
+      var buttonElt3 = document.getElementsByTagName('button')[3];
+      var replaceButton3 = document.createElement('span');
+
+      replaceButton3.innerHTML = buttonElt3.innerHTML;
+      buttonElt3.parentNode.replaceChild(replaceButton3,buttonElt3);
+      replaceButton3.innerHTML = 'Incorrect!';
+      replaceButton3.className += 'incorrect';
+      replaceButton3.style.fontSize = '1.8em';
+      replaceButton3.style.paddingTop = '40px';
+
+
       console.log('Incorrect! Your position is: ' + thirdPos + ' and the ball is at position: ' + positionChoice);
     }
-
-
-    // console.log('cup3 here!');
-    // console.log('This is position: ' + positionChoice);
   });
+
 
   //when the 'Let's Play' button is pressed, hide the rules and show the cups'
   play.addEventListener('click', function() {
@@ -91,6 +159,7 @@ document.addEventListener("DOMContentLoaded", function(){
     showRules.style.display = 'block';
   });
 
+
   //when the 'Show Rules' button is pressed, hide the cups and show the rules'
   showRules.addEventListener('click', function () {
     hideRules.style.display = 'block';
@@ -98,6 +167,5 @@ document.addEventListener("DOMContentLoaded", function(){
     showCups.style.display = 'none';
     showRules.style.display = 'none';
   });
-
 
 });
