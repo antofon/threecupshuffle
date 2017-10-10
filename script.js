158d31da4d33efe1eb228ff6156fcba6eb9e0000
Anwana Ntofon <aontofon@ucdavis.edu>
2017-10-09 21:27:58 -0700

    Shuffle game works properly
    All button elements respond as intended. Can guess all three times to
    determine where the ball is located. Previous commented code removed.
    Reshuffles on page refresh.

--- script.js
+++ script.js
@@ -8,25 +8,31 @@ document.addEventListener("DOMContentLoaded", function(){
   var cup1 = document.getElementById('cup1');
   var cup2 = document.getElementById('cup2');
   var cup3 = document.getElementById('cup3');
+  //firstPos and firstPosIndex needed; firstPos will not change in value because that is the position that the user thinks the ball is at. firstPosIndex or the other PosIndex vars will change because upon clicking a button, this will reduce the total button count since the button is being replaced with text in a span. Therefore, the PosIndex needs to be decremented and tailored to the current state of the PosIndex for the remaining buttons. Only decrement at stages that makes sense. firstPosIndex will never be decremented, only accessed.
+
   var firstPos = 1;
+  var firstPosIndex =  1;
   var secondPos = 2;
+  var secondPosIndex = 2;
   var thirdPos = 3;
+  var thirdPosIndex = 3;
   var hideRules = document.getElementById('rules');
   var oneChance =  0;
   var play = document.getElementById('play');
   var showRules = document.getElementById('fixed-rules');
   var showCups = document.getElementById('begin');
   //will be used for random position of ball
-
-var positionChoice = (Math.floor(Math.random() * 3)) + 1;
-
+  var positionChoice = (Math.floor(Math.random() * 3)) + 1;
 
 
   function guessFirstPos() {
     //if the user's guess equals the random position of the ball
     if(firstPos === positionChoice) {
+      //in either statement that the user guesses, the other PosIndex will have to be decremented because the button is being replaced and lowering the total button length.
+      secondPosIndex--;
+      thirdPosIndex--;
       //accesses second element(note, position 0 is the first element, which is the 'Show Rules' button)
-      var buttonElt = document.getElementsByTagName('button')[1];
+      var buttonElt = document.getElementsByTagName('button')[firstPosIndex];
       //create new element (span)
       var replaceButton = document.createElement('span');
 
@@ -60,8 +66,10 @@ var positionChoice = (Math.floor(Math.random() * 3)) + 1;
     }
 
     else {
+      secondPosIndex--;
+      thirdPosIndex--;
       //same result as 'if' statement, but do not display ball, only 'incorrect' message
-      var buttonElt = document.getElementsByTagName('button')[1];
+      var buttonElt = document.getElementsByTagName('button')[firstPosIndex];
       var replaceButton = document.createElement('span');
 
       if(buttonElt && replaceButton !== null){
@@ -82,14 +90,14 @@ var positionChoice = (Math.floor(Math.random() * 3)) + 1;
 
       console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
   }
-  }
-
-
+}
 
 
 function guessSecondPos() {
   if(secondPos === positionChoice) {
-      var buttonElt2 = document.getElementsByTagName('button')[2];
+
+    thirdPosIndex--;
+      var buttonElt2 = document.getElementsByTagName('button')[secondPosIndex];
       var replaceButton2 = document.createElement('span');
       if(buttonElt2 && replaceButton2 !== null) {
         console.log("not empty.");
@@ -116,8 +124,9 @@ function guessSecondPos() {
     }
 
     else {
+      thirdPosIndex--;
       //same result as 'if' statement, but do not display ball, only 'incorrect' message
-      var buttonElt2 = document.getElementsByTagName('button')[2];
+      var buttonElt2 = document.getElementsByTagName('button')[secondPosIndex];
       var replaceButton2 = document.createElement('span');
 
       if(buttonElt2 && replaceButton2 !== null) {
@@ -126,6 +135,7 @@ function guessSecondPos() {
       }
 
       else {
+
         console.log(buttonElt2 + " " + replaceButton2);
       }
 
@@ -143,7 +153,7 @@ function guessSecondPos() {
 
 function guessThirdPos () {
   if(thirdPos === positionChoice) {
-      var buttonElt3 = document.getElementsByTagName('button')[2];
+      var buttonElt3 = document.getElementsByTagName('button')[thirdPosIndex];
       var replaceButton3 = document.createElement('span');
       if(buttonElt3 && replaceButton3 !== null) {
         console.log(buttonElt3 + " " + replaceButton3);
@@ -170,7 +180,7 @@ function guessThirdPos () {
 
     else {
       //same result as 'if' statement, but do not display ball, only 'incorrect' message
-      var buttonElt3 = document.getElementsByTagName('button')[2];
+      var buttonElt3 = document.getElementsByTagName('button')[thirdPosIndex];
       var replaceButton3 = document.createElement('span');
       if(buttonElt3 && replaceButton3 !== null) {
         console.log("not empty.");
@@ -188,249 +198,13 @@ function guessThirdPos () {
       replaceButton3.style.fontSize = '1.8em';
       replaceButton3.style.paddingTop = '40px';
 
-
       console.log('Incorrect! Your position is: ' + thirdPos + ' and the ball is at position: ' + positionChoice);
     }
-
 }
 
-
-    if(cup1Button.addEventListener('click', guessFirstPos)) {
-      cup2Button.disabled = true;
-     cup3Button.disabled = true;
-    }
-
-    else if(cup2Button.addEventListener('click', guessSecondPos)) {
-      cup1Button.disabled = true;
-     cup3Button.disabled = true;
-    }
-
-    else if(cup3Button.addEventListener('click', guessThirdPos)) {
-      cup1Button.disabled = true;
-     cup2Button.disabled = true;
-    }
-    // switch (userPos) {
-    //   case 1:
-    //   cup2Button.disabled = true;
-    //   cup3Button.disabled = true;
-    //     cup1Button.addEventListener('click', guessFirstPos);
-    //     break;
-    //
-    //   case 2:
-    //   cup1Button.disabled = true;
-    //   cup3Button.disabled = true;
-    //     cup2Button.addEventListener('click', guessSecondPos);
-    //     break;
-    //
-    //   case 3:
-    //   cup1Button.disabled = true;
-    //   cup2Button.disabled = true;
-    //     cup3Button.addEventListener('click', guessThirdPos);
-    //     break;
-    //   default:
-    //
-    // }
-
-
-  //   cup1Button.addEventListener('click', guessFirstPos);
-  //
-  //
-  //
-  //   cup3Button.addEventListener('click', guessThirdPos);
-  //
-  //   // cup2Button.addEventListener('click', guessSecondPos(secondPos, positionChoice));
-  //   //
-  //   // cup3Button.addEventListener('click', guessThirdPos(thirdPos, positionChoice));
-  //
-  //   oneChance++;
-  // }
-
-
-
-
-
-
-  // cup1Button.addEventListener('click',function() {
-  //   var firstPos = 1;
-  //
-  //   //if the user's guess equals the random position of the ball
-  //   if(firstPos === positionChoice) {
-  //     //accesses second element(note, position 0 is the first element, which is the 'Show Rules' button)
-  //     var buttonElt = document.getElementsByTagName('button')[1];
-  //     //create new element (span)
-  //     var replaceButton = document.createElement('span');
-  //
-  //     if(buttonElt && replaceButton !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt + " " + replaceButton);
-  //     }
-  //
-  //     //store the button's contents into replaceButton var (which will be used)
-  //     replaceButton.innerHTML = buttonElt.innerHTML;
-  //     //replace the child element of the parent node. Now we are replacing the old element's info (buttonElt), with the new element's info (replaceButton)
-  //     buttonElt.parentNode.replaceChild(replaceButton,buttonElt);
-  //     //modifying new element's text
-  //     replaceButton.innerHTML = 'Correct!';
-  //     //adding the 'correct' class
-  //     replaceButton.className += 'correct';
-  //     replaceButton.style.fontSize = '1.8em';
-  //     replaceButton.style.paddingTop = '40px';
-  //
-  //     //access 'src' attribute of cup1 ID (which is for the first red cup) and overwrite with ball SVG
-  //     cup1.src = 'metallic_ball.svg';
-  //     cup1.style.width = '100px';
-  //     cup1.style.height = '100px';
-  //     cup1.style.marginTop = '80px';
-  //
-  //     console.log('Correct! Your position is: ' + firstPos + ' and the ball is also at position: ' + positionChoice);
-  //   }
-  //
-  //   else {
-  //     //same result as 'if' statement, but do not display ball, only 'incorrect' message
-  //     var buttonElt = document.getElementsByTagName('button')[1];
-  //     var replaceButton = document.createElement('span');
-  //
-  //     if(buttonElt && replaceButton !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt + " " + replaceButton);
-  //     }
-  //
-  //     replaceButton.innerHTML = buttonElt.innerHTML;
-  //     buttonElt.parentNode.replaceChild(replaceButton,buttonElt);
-  //     replaceButton.innerHTML = 'Incorrect!';
-  //     replaceButton.className += 'incorrect';
-  //     replaceButton.style.fontSize = '1.8em';
-  //     replaceButton.style.paddingTop = '40px';
-  //
-  //     console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
-  //     //cup1.style.display = 'none';
-  //   //   var playAgain = prompt("Would you like to play again? (Yes or No)");
-  //   //   if(playAgain.toUpperCase() === 'YES') {
-  //   //     alert("Great! Please refresh the page to begin a new game.");
-  //   //   }
-  //   //
-  //   //   else {
-  //   //     alert("Okay, see you next time!");
-  //   //   }
-  //   //   console.log('Incorrect! Your position is: ' + firstPos + ' and the ball is at position: ' + positionChoice);
-  //    }
-  //});
-
-
-
-
-  //
-  //
-  // cup2Button.addEventListener('click', function() {
-  //   var secondPos = 2;
-  //   if(secondPos === positionChoice) {
-  //     var buttonElt2 = document.getElementsByTagName('button')[2];
-  //     var replaceButton2 = document.createElement('span');
-  //     if(buttonElt2 && replaceButton2 !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt2 + " " + replaceButton2);
-  //     }
-  //
-  //     replaceButton2.innerHTML = buttonElt2.innerHTML;
-  //     buttonElt2.parentNode.replaceChild(replaceButton2,buttonElt2);
-  //     replaceButton2.innerHTML = 'Correct!';
-  //     replaceButton2.className += 'correct';
-  //     replaceButton2.style.fontSize = '1.8em';
-  //     replaceButton2.style.paddingTop = '40px';
-  //
-  //     cup2.src = 'metallic_ball.svg';
-  //     cup2.style.width = '100px';
-  //     cup2.style.height = '100px';
-  //     cup2.style.marginTop = '80px';
-  //
-  //     console.log('Correct! Your position is: ' + secondPos + ' and the ball is also at position: ' + positionChoice);
-  //   }
-  //
-  //   else {
-  //     //same result as 'if' statement, but do not display ball, only 'incorrect' message
-  //     var buttonElt2 = document.getElementsByTagName('button')[2];
-  //     var replaceButton2 = document.createElement('span');
-  //
-  //     if(buttonElt2 && replaceButton2 !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt2 + " " + replaceButton2);
-  //     }
-  //
-  //     replaceButton2.innerHTML = buttonElt2.innerHTML;
-  //     buttonElt2.parentNode.replaceChild(replaceButton2,buttonElt2);
-  //     replaceButton2.innerHTML = 'Incorrect!';
-  //     replaceButton2.className += 'incorrect';
-  //     replaceButton2.style.fontSize = '1.8em';
-  //     replaceButton2.style.paddingTop = '40px';
-  //
-  //     console.log('Incorrect! Your position is: ' + secondPos + ' and the ball is at position: ' + positionChoice);
-  //   }
-  // });
-  //
-  //
-  // cup3Button.addEventListener('click', function() {
-  //   var thirdPos = 3;
-  //   if(thirdPos === positionChoice) {
-  //     var buttonElt3 = document.getElementsByTagName('button')[3];
-  //     var replaceButton3 = document.createElement('span');
-  //     if(buttonElt3 && replaceButton3 !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt3 + " " + replaceButton3);
-  //     }
-  //     replaceButton3.innerHTML = buttonElt3.innerHTML;
-  //     buttonElt3.parentNode.replaceChild(replaceButton3,buttonElt3);
-  //     replaceButton3.innerHTML = 'Correct!';
-  //     replaceButton3.className += 'correct';
-  //     replaceButton3.style.fontSize = '1.8em';
-  //     replaceButton3.style.paddingTop = '40px';
-  //
-  //     cup3.src = 'metallic_ball.svg';
-  //     cup3.style.width = '100px';
-  //     cup3.style.height = '100px';
-  //     cup3.style.marginTop = '80px';
-  //
-  //     console.log('Correct! Your position is: ' + thirdPos + ' and the ball is also at position: ' + positionChoice);
-  //   }
-  //
-  //   else {
-  //     //same result as 'if' statement, but do not display ball, only 'incorrect' message
-  //     var buttonElt3 = document.getElementsByTagName('button')[3];
-  //     var replaceButton3 = document.createElement('span');
-  //     if(buttonElt3 && replaceButton3 !== null) {
-  //       console.log("not empty.");
-  //     }
-  //
-  //     else {
-  //       console.log(buttonElt3 + " " + replaceButton3);
-  //     }
-  //
-  //     replaceButton3.innerHTML = buttonElt3.innerHTML;
-  //     buttonElt3.parentNode.replaceChild(replaceButton3,buttonElt3);
-  //     replaceButton3.innerHTML = 'Incorrect!';
-  //     replaceButton3.className += 'incorrect';
-  //     replaceButton3.style.fontSize = '1.8em';
-  //     replaceButton3.style.paddingTop = '40px';
-  //
-  //
-  //     console.log('Incorrect! Your position is: ' + thirdPos + ' and the ball is at position: ' + positionChoice);
-  //   }
-  // });
-//  }
+    cup1Button.addEventListener('click', guessFirstPos);
+    cup2Button.addEventListener('click', guessSecondPos);
+    cup3Button.addEventListener('click', guessThirdPos);
 
 
   //when the 'Let's Play' button is pressed, hide the rules and show the cups'
@@ -439,16 +213,6 @@ function guessThirdPos () {
     play.style.display = 'none';
     showCups.style.display = 'block';
     showRules.style.display = 'block';
-
-    // var userPos = prompt("What position would you like to guess? (Must be a number from 1 to 3)");
-    // guessPos
-    // if(parseInt(userPos) !== parseInt(userPos)) {
-    //   console.log("Not a number. Refresh the page to try again.")
-    // }
-    //
-    // else {
-    // console.log("Number is: " + userPos);
-    // }
   });
 
 
